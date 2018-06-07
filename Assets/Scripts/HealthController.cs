13a4c38420a44b8a4d9019f7d44aec868f048ac7
Simon Garske <simon.garske@r132-04-w10.SCHULE-ISERV>
2018-06-07 14:40:46 +0200

    schreiben

---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthController : MonoBehaviour {
+	public float health = 5;
+	private bool isDead = false;
+
+	void ApplyDamage(float damage)
+	{
+		health -= damage;	
+		
+		if(health <= 0 && !isDead)
+		{
+			isDead = true;
+			Dying();
+		}
+		else
+		{
+			Damaging();
+		}
+
+		Debug.Log("health = " + health);
+	}
+
+	public virtual void Damaging()
+	{		
+	}
+	
+	public virtual void Dying ()
+	{
+	}
+}
